1407a5b1120b8a0c62facbcb04d326646af29a37
Filigs <40748182+Filigs@users.noreply.github.com>
2024-01-13 16:16:28 +0000

    update carousel

--- app/Carousel.js
+++ app/Carousel.js
@@ -1,5 +1,28 @@
 "use client";
+import styles from "../styles/Carousel.module.css";
+import Image from "next/image";
+import pop from "../public/images/carousel/pop.svg";
 
 export default function Carousel() {
-  return <>Hello</>;
+  const filler = () => {
+    for (let i = 0; i < 20; i++) i = i + 1;
+  };
+  return (
+    <div className={styles.container}>
+      {!filler && (
+        <div className={styles.card}>
+          <div className={styles.imageContainer}>
+            <Image
+              src={pop}
+              alt="Pop Music"
+              width={30}
+              height={30}
+              className={styles.image}
+            />
+          </div>
+          <h4>Estilo</h4>
+        </div>
+      )}
+    </div>
+  );
 }
--- app/home.js
+++ app/home.js
@@ -1,9 +1,11 @@
 "use client";
 import styles from "../styles/Home.module.css";
+import Carousel from "./Carousel";
 export default function Homepage() {
   return (
     <div className={`${styles.container} min-h-screen bg-primary-darker`}>
       <h1>Hello world</h1>
+      <Carousel />
     </div>
   );
 }
